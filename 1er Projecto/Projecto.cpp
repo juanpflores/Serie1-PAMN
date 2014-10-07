8ff19761208dbff5199691eb93b107c77793c19a
juanpflores94 <juanpflores94@gmail.com>
2014-10-07 09:49:53 -0500

    Update del projecto
    Agregue el metodo random y algunas variables

--- 1er Projecto/Projecto.cpp
+++ 1er Projecto/Projecto.cpp
@@ -7,16 +7,38 @@
 	Equipo:
 	-Juan Pablo Flores 
 */
+int random()
 
 void main(){
 	//Declaracion de variables
+	FILE *PRINT;
+	int *numaleatorios;
+	numaleatorios=(int*)malloc(100*sizeof(int));
+	
+	PRINT=fopen("resultados.xls", "w");
 	char nombre[10];
-	int numeros[2], numaleatorios[100];
+	int numeros[2];
+	
 	
 	//Inicio del programa
 	printf("Bienvenido a Juego \n Ingresa tu nombre:");
 	scanf("%d", &nombre);
+	printf("Escoge tres numeros");
 	
-	for(int i=0; i<2;)
+	for(int i=0; i<2; i++){
+		printf("Escribe que numero entre 1 y 100 quieres para tu numero "+(i+1))
+		scanf("%d",&numero[i]);
+	}
 	
+	//Iniciamos a poner los numeros aleatorios.
+	
+	
+}
+
+int random(){
+	int number, input;
+		
+		srand ( time(NULL) );
+		number = rand() % 100 + 1;
+		return number;
 }
