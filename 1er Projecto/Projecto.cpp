ccec9c80e41c8f83835a8a63c3098eeb1ec390ba
juanpflores94 <juanpflores94@gmail.com>
2014-10-07 09:25:33 -0500

    Cree el projecto del primer Parcial

---
+++ 1er Projecto/Projecto.cpp
@@ -0,0 +1,22 @@
+<stdlib.h>
+<stdio.h>
+<math.h> //Para la funcion random
+/*
+	Primer Projecto de PAMN
+	
+	Equipo:
+	-Juan Pablo Flores 
+*/
+
+void main(){
+	//Declaracion de variables
+	char nombre[10];
+	int numeros[2], numaleatorios[100];
+	
+	//Inicio del programa
+	printf("Bienvenido a Juego \n Ingresa tu nombre:");
+	scanf("%d", &nombre);
+	
+	for(int i=0; i<2;)
+	
+}
--- Java/Hola.java
+++ Java/Hola.java
@@ -1,8 +1,16 @@
-public class Hola{
-	//atributos
-	//metodos
+public class Cuates{
+	String nombre;
+	int edad;
+	double promedio;
 
 	public static void main(String args[]){
-		System.out.println("Sup Java");
+		Cuates c1=new Cuates("Manuel",28,9.98);
+		Cuates c2=new Cuates("Gloria",20,8.5);
+	}
+	public Cuates(String nom, int ed, double prom){
+		this.nombre=nom;
+		this.edad=ed;
+		this.promedio-prom;
+		System.out.println("Se ha creado un amigo.");
 	}
 }
