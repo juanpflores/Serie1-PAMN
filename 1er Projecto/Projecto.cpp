97182ca85750f5ee37d58d0bb33ad5184aa333c7
juanpflores94 <juanpflores94@gmail.com>
2014-10-07 18:13:27 -0500

    Random Listo

--- 1er Projecto/Projecto.cpp
+++ 1er Projecto/Projecto.cpp
@@ -1,15 +1,16 @@
-<stdlib.h>
-<stdio.h>
-<math.h> //Para la funcion random
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+//#include <math.h> //Para la funcion random
 /*
 	Primer Projecto de PAMN
 	
 	Equipo:
 	-Juan Pablo Flores 
 */
-int random()
+int random();
 
-void main(){
+	main(){
 	//Declaracion de variables
 	FILE *PRINT;
 	int *numaleatorios;
@@ -17,7 +18,7 @@ void main(){
 	
 	PRINT=fopen("resultados.xls", "w");
 	char nombre[10];
-	int numeros[2];
+	int numeros[2],solucion=0,confirm=0;
 	
 	
 	//Inicio del programa
@@ -26,13 +27,25 @@ void main(){
 	printf("Escoge tres numeros");
 	
 	for(int i=0; i<2; i++){
-		printf("Escribe que numero entre 1 y 100 quieres para tu numero "+(i+1))
-		scanf("%d",&numero[i]);
+		printf("Escribe que numero entre 1 y 100 quieres para tu numero "+(i+1));
+		scanf("%d",&numeros[i]);
 	}
 	
 	//Iniciamos a poner los numeros aleatorios.
-	
-	
+	for(int i=0; i<100||solucion>3;i++){
+		numaleatorios[i]=random();
+		fflush(stdin);
+		printf("Tu numero es el siguiente? %d\t\t\t si(1)\tno(0) ", numaleatorios[i]);
+		scanf(" %d", &confirm);
+		if(confirm==1){
+			for(int j =0;j<2;j++){
+				//if(numeros[j]==numaleatorio[i]){
+					
+			//	}
+			}
+		}
+		
+	}	
 }
 
 int random(){
